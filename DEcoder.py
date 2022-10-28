e0f2d6064b63f893c114b139c01965096abad559
84mu3lC3p4C7 <111708236+84mu3lC3p4C7@users.noreply.github.com>
2022-10-28 23:42:35 +0200

    Update DEcoder.py

--- DEcoder.py
+++ DEcoder.py
@@ -1,10 +1,13 @@
-print("Welcome to 84´s (de)coder v 1.1")
+print("Welcome to 84´s (de)coder v 1.2\nType 'c' for coding or 'd' for decoding")
 def selecting_action():
-    selected = input("Plese select an action: (c/d) ")
+    selected = input("Select an action: (c/d) ")
     if selected == "c":
         coding_the_number()
     if selected == "d":
        decoding_the_number()
+    else:
+        print("Invalid input")
+        selecting_action()
 
 def run_again():
     again = input("(yes/no) ")
@@ -20,10 +23,9 @@ def coding_the_number():
     looking_for_number = 0
     search_start = 0
     number_found_at = ""
-    added_0 = False
     code = input("Enter number you want to code: ")
     code_lenght = len(code)
-
+    
     while len(dictionary_positions) != code_lenght:
         number_found_at = code.find(str(looking_for_number), search_start)
         if number_found_at != -1:
@@ -33,13 +35,7 @@ def coding_the_number():
             looking_for_number += 1
             search_start = 0
 
-    code_lenght = len(code)
-    if (code_lenght % 2) == 1:
-        dictionary_positions[code_lenght] = 0
-        code_lenght += 1
-        added_0 = True
-
-    #print("Dictionary before math " + str(dictionary_positions) + ",added 0: " + str(added_0) + ", lenght = "+ str(code_lenght))
+    #print("Dictionary before math " + str(dictionary_positions) + ", lenght = "+ str(code_lenght))
     position = 0
     code_lenght_copy = code_lenght + 1
     while code_lenght_copy != 1:
@@ -53,8 +49,10 @@ def coding_the_number():
     from_end = code_lenght - 1
     from_start = 0
     mixed_positions = ""
-    while from_end > from_start:
-        mixed_positions += str(c_dictionary_positions[from_end])+str(c_dictionary_positions[from_start])
+    while from_end >= from_start:
+        mixed_positions += str(c_dictionary_positions[from_end])
+        if from_start != from_end:
+            mixed_positions += str(c_dictionary_positions[from_start])
         from_start += 1
         from_end -= 1
         
@@ -77,14 +75,15 @@ def decoding_the_number():
         start_n += 3
         end_n += 3
     #print("Unsorted numbers: " + str(d_dictionary_positions))
-
+    
     from_end = int(decode_lenght) - 1
     from_start = 0
     position = 0
     while len(d_dictionary_positions) != len(rp_dictionary_positions):
         rp_dictionary_positions[from_end] = d_dictionary_positions[position]
         position += 1
-        rp_dictionary_positions[from_start] = d_dictionary_positions[position]
+        if len(d_dictionary_positions) != len(rp_dictionary_positions):
+            rp_dictionary_positions[from_start] = d_dictionary_positions[position]
         position += 1
         from_end -= 1
         from_start += 1
@@ -99,8 +98,7 @@ def decoding_the_number():
         position += 1
         decoded += str(decoded_answer)
 
-
-    print("You number has been decoded to: " + str(decoded)  + " (there may be extra '0' at the end of the number)" + "\nAgain?")
+    print("You number has been decoded to: " + str(decoded) + "\nAgain?")
     run_again()
 
 selecting_action()
